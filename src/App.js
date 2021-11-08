12bd3054f48168efcff69e5eb6bc9daa21f6daab
Alin Manole <alin.manole@yahoo.com>
2021-11-08 15:12:17 +0100

    Titre du site + Container / Appel Scores.js / GridPlayer.JS / GridIA.js/ Message.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-class App  extends React.Component {
-  render() { 
+class App extends React.Component {
+  render() {
     return (
-    <div>
-      <h1>salut</h1>
-    </div>
+      <div className="container">
+        <h1>BattleShip Game</h1>
+        {/* <Score />
+        <MainGrid>
+          <GridPlayer />
+          <GridIA />
+        </MainGrid>
+        <Message /> */}
+      </div>
     )
   }
 }
- 
-export default App ;
+
+export default App;
 
