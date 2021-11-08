cbe03267efca450b4f9116662b160cc54b81d891
Alin Manole <alin.manole@yahoo.com>
2021-11-08 21:25:14 +0100

    ButtonShips

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ButtonShips from './components/ButtonShips';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <h1>BattleShip Game</h1>
-        {/* <Score />
-        <MainGrid>
-          <GridPlayer />
-          <GridIA />
-        </MainGrid>
-        <Message /> */}
+        {/* <Score /> */}
+        {/* <MainGrid> */}
+          {/* <GridPlayer /> */}
+          {/* <GridIA /> */}
+        {/* </MainGrid> */}
+        {/* <Message /> */}
+        <ButtonShips />
       </div>
     )
   }
