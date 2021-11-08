31ed8189e492bb9459aed2ba9ff7360509d583db
Nascimento95 <marco-pereira@hotmail.fr>
2021-11-08 15:14:52 +0100

    debut de la création du grid

---
+++ src/components/Grid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+class Grid
+ extends React.Component {
+    render() { 
+        return (
+            <div className="row">
+                <div className="col-1">
+
+                </div>
+            </div>
+        ) 
+    }
+}
+ 
+export default Grid
+;
