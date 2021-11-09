8f8edd8af6e5d3eab308a14238624f8e648a33d7
Nascimento95 <marco-pereira@hotmail.fr>
2021-11-09 16:12:54 +0100

    selection des bateau pour le player

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -17,31 +17,33 @@ class Grid
                 ["9","","","","","","","","","",""],
                 ["10","","","","","","","","","",""],
             ]
+
         } 
             
      }
 
-     setBoat(i, j){
-
-        console.log(i, j)
-        // const array =  i + ship , j
-     }
-
     render() { 
-        console.log(this.props)
-        const { updateBoatPosition } = this.props
+        // ici les props passer dans grid dans app.js
+        const { updateBoatPosition, destroyer, cruiser, submarine, carrier } = this.props
+        // dans le maps nous avons 4 variable const (des ternaire) qui permette de vérifier si les coordonné d un bateau corresponde a la coordonné de la case cliquer et qui lui pase un background gris dans classname
         return (
             
             <div className="container-fluid mt-5 mx-5 ">
                 {this.state.grid.map((row,i) => (  
                     <div key={i} className="row">
-                        {row.map((col,j) => (
-                        <div key={j} onClick={() => updateBoatPosition(i, j)}className="col-1 py-2  border border-1">
-                            <div>
-                                <h6>{col}</h6>
-                            </div>
-                        </div>
-                        ))}
+                        {row.map((col,j) => {
+                            // les ternaires pour le backgroud de la case selectionner
+                            const carrierClass = carrier.includes(`${i}${j}`) ? `bg-secondary`  : ""
+                            const submarineClass = submarine.includes(`${i}${j}`) ? `bg-secondary`  : ""
+                            const destroyerClass = destroyer.includes(`${i}${j}`) ? `bg-secondary`  : ""
+                            const cruiserClass = cruiser.includes(`${i}${j}`) ? `bg-secondary`  : ""
+                            return(
+                                <div key={j} onClick={() => updateBoatPosition(i, j)}className={`col-1 py-2 ${destroyerClass} ${cruiserClass} ${submarineClass} ${carrierClass} border border-1`}>
+                                    <div>
+                                        <h6>{col}</h6>
+                                    </div>
+                                </div>
+                        )})}
                     </div>
                 ))}
                 
