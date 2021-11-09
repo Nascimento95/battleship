aca9a41f2bf00a8a6bc40634c477b7b35cfa484e
Alin Manole <alin.manole@yahoo.com>
2021-11-09 16:36:25 +0100

    update Marko

--- src/App.js
+++ src/App.js
@@ -23,25 +23,58 @@ class App extends React.Component {
   }
 
   handleChooseShip(e) {
-    console.log(e)
+    // console.log(e)
     this.setState({ selectedboat: e })
   }
-
+  // la function updateboatPosition Recupere en parametre les index des div de mon state grid dans grid.js
+  // avec des condition elle nous dit si nous avon selection le bouton destroyeur ,carier etc ...
+  // et apres sa nous permet de push dans le tableau correspondant au boutton les valeur des parametre de la function qui sont case cliquer
+  // nous avon aussi des condition qui nous permette de ne pas depasser le nombre de case que correspond le bateau choisi
   updateBoatPosition(i, j){
-    this.setState({destroyer: i, j})
+    const {selectedboat} =this.state
+    const compareDestroyeur = ["1","1"]
+    const compareCruiser = ["1","1","1"]
+    const compareSubmarine = ["1","1","1","1"]
+    const compareCarrier = ["1","1","1","1","1"]
+
+    if(selectedboat === "destroyer"){
+      this.setState({destroyer: [`${i}${j}`, ...this.state.destroyer]});
+      if(this.state.destroyer.length >= compareDestroyeur.length){
+        this.setState({destroyer: this.state.destroyer});
+      }
+    } else if(selectedboat === "cruiser"){
+      this.setState({cruiser: [`${i}${j}`, ...this.state.cruiser]})
+      if(this.state.cruiser.length >= compareCruiser.length){
+        this.setState({cruiser: this.state.cruiser});
+      }
+    } else if(selectedboat === "submarine"){
+      this.setState({submarine: [`${i}${j}`, ...this.state.submarine]})
+      if(this.state.submarine.length >= compareSubmarine.length){
+        this.setState({submarine: this.state.submarine});
+      }
+    } else if(selectedboat === "carrier"){
+      this.setState({carrier: [`${i}${j}`, ...this.state.carrier]})
+      if(this.state.carrier.length >= compareCarrier.length){
+        this.setState({carrier: this.state.carrier});
+      }
+    }
   
   }
 
   render() {
-    // console.log(this.state.selectedboat)
-    console.log("This is it " , this.state.destroyer)
+    
     return (
       <>
         <h1 className="text-center">BattleShip Game</h1>
 
         <div className="d-flex">
-          <Grid updateBoatPosition={this.updateBoatPosition} />
-          <Grid />
+          <Grid  
+          carrier={this.state.carrier}
+          submarine={this.state.submarine}
+          cruiser={this.state.cruiser} 
+          destroyer={this.state.destroyer} 
+          updateBoatPosition={this.updateBoatPosition} />
+          {/* <Grid /> */}
         </div>
         <ButtonShips text="destroyer" onClick={this.handleChooseShip} />
         <ButtonShips text="cruiser" onClick={this.handleChooseShip} />
@@ -54,4 +87,3 @@ class App extends React.Component {
 }
 
 export default App;
-
--- src/components/ButtonShips.js
+++ src/components/ButtonShips.js
@@ -3,33 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 class ButtonShips extends Component {
-    //     constructor (){
-    //         super()
-
-    //         this.state = {
-    //             destroyer: 2,
-    //             cruiser: 3,
-    //             submarine: 4,
-    //             carrier: 5
-    //         }
-            
-    //         this.chooseShip = this.handleChooseShip.bind(this)
-    //     }
-
-    // handleChooseShip(e) {
-    //     console.log(e)
-    // }
-
-
+    
     render() {
-        console.log(this.state)
+        
         const {text, onClick } = this.props
         return (
             <>
-                <div shipname="destroyer" onClick={()=>onClick(text)} className="btn btn-primary" >{text}</div>
-                {/* <div shipname="cruiser" className="btn btn-primary" onClick={()=> this.handleChooseShip(this.state.cruiser)}>Cruiser</div>
-                <div shipname="submarine" className="btn btn-primary" onClick={()=> this.handleChooseShip(this.state.submarine)}>Submarine</div>
-                <div shipname="carrier" className="btn btn-primary" onClick={()=> this.handleChooseShip(this.state.carrier)}>Aircraft Carrier</div> */}
+                <div shipname={text} onClick={()=>onClick(text)} className="btn btn-primary" >{text}</div>
             </>
         );
     }
